4dd2b073f1ee4ae081c3d9276871580532e3478c
jamesmartherus <james@martherus.com>
2019-09-24 06:22:41 -0500

    Fix geom_shotplot

--- R/geom_shotplot.R
+++ R/geom_shotplot.R
@@ -2,7 +2,7 @@
 #' 
 #' Creates a shot location plot. Calls geom_point() with an image
 #' of an NHL rink as the background. 
-#' 
+#' @param ... Passed to [ggplot2::geom_point()]
 #' @keywords ggplot geom
 #' @examples
 #' library(ggplot2)
@@ -14,10 +14,10 @@
 #' @import png
 #' @import grid
 #' @export
-image <- png::readPNG("man/figures/full-rink.png")
 geom_shotplot <- function(...){
-  annotation_custom(grid::rasterGrob(image, 
+  list(annotation_custom(grid::rasterGrob(png::readPNG("man/figures/full-rink.png"), 
                                width = unit(1,"npc"), 
-                               height = unit(1,"npc"))) + geom_point() + coord_flip()
+                               height = unit(1,"npc"))),
+       geom_point(),
+       coord_flip())
 }
-
